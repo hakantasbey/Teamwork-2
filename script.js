ce749d8ea3bd32411c93083e12a05b1799b387b0
hakan <tasbeyhakan@gmail.com>
2023-10-03 22:30:23 +0300

    first commit

--- script.js
+++ script.js
@@ -29,5 +29,30 @@
 //     console.log(`${year} is not a leap year.`);
 // }
 
+//? -------- Aylarin kac gun oldugu--------
 
+let month = prompt("enter a month").toLowerCase();
+month = month.toLowerCase();
+switch (month) {
+  case "ocak":
+  case "mart":
+  case "mayis":
+  case "temmuz":
+  case "agustos":
+  case "ekim":
+  case "aralik":
+    console.log(`${month} has a 31 days`);
+    break;
+  case "nisan":
+  case "haziran":
+  case "eylul":
+  case "kasim":
+    console.log(`${month} has a 30 days`);
+    break;
+  case "subat":
+    console.log(`${month} has a 28 days`);
+    break;
+  default:
+    console.log("hatali bir giris yaptiniz");
+}
 
