34ca8d779e52ce49c864617c9dde47290e1d2299
hakan <tasbeyhakan@gmail.com>
2023-10-03 22:27:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// ?-------------Get Season----------------
+
+
+// let month = prompt("enter a month: 1/12")
+// let day = prompt("enter a day: 1/31")
+
+// if(!(month>=1 && month<=12) || !(day>=1 && day<=31)){
+//     console.log("ERROR");
+// } else{
+// let season 
+// if((month>=3 && month<=5) || (month == 2 && day>=21 && day<=29)){
+//     season = "SPRING"
+// } else if((month>=6 && month<=8) || (month == 9 && day<=21 )){
+//     season = "SUMMER"
+// } else if((month>=10 && month<=11) || (month==9 && day>=22) || (month == 12 && day<=20)){
+//     season = "FALL"
+// } else if((month==1) || (month == 12 && day>=21) || (month==2 && day<=20)){
+//     season = "WINTER"
+// }
+// console.log(`The season for ${month} / ${day} is ${season}`);
+// }
+
+// ?---------Leap Year------------
+
+// let year = prompt("Please enter a year")
+// if(((year % 4 == 0)&& !(year % 100 == 0)) || (year % 400 == 0)){
+//     console.log(`${year} is a leap year.`);
+// }else{
+//     console.log(`${year} is not a leap year.`);
+// }
+
+
+
